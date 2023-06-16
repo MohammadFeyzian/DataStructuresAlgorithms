3a3bd243ea6f71ba488f7aec6896319d5f980bf2
Mohammad <mohammad@curatio.me>
2023-06-15 17:53:03 -0700

    Dynamic Programing, Memoize and Cache

---
+++ src/main/kotlin/algorithm/section15_dynamic_programming/1_memoize_cache.kt
@@ -0,0 +1,40 @@
+package algorithm.section15_dynamic_programming
+
+/**
+ * In some cases we have one operation which is happening multiple times
+ * ,and it is doing the same thing.
+ *
+ * To optimize the operation and preventing some redundant operations,
+ * we can cache the result and return it in the same situations.
+ */
+fun main() {
+
+    val memoizeAdd = MemoizeAdd()
+
+    println(memoizeAdd.addTo80(5))
+    println(memoizeAdd.addTo80(5))
+    println(memoizeAdd.addTo80(5))
+    println(memoizeAdd.addTo80(4))
+    println(memoizeAdd.addTo80(2))
+    println(memoizeAdd.addTo80(5))
+}
+
+private class MemoizeAdd {
+    val cache = mutableMapOf<Int, Int>()
+
+    fun addTo80(n: Int): Int {
+        return memoizeAddTo80(n)
+    }
+
+    private fun memoizeAddTo80(n: Int): Int {
+        return if (cache.contains(n)) {
+            println("Caching")
+            cache[n]!!
+        } else {
+            println("Long operation is happening")
+            val operation = n + 80
+            cache[n] = operation
+            operation
+        }
+    }
+}
